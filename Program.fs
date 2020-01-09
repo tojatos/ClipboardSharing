72bf3bf841da8899a50d37e7574b7f6e56fd7cc6
tojatos <tojatos@gmail.com>
2020-01-10 00:37:19 +0100

    init

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    TextCopy.Clipboard.SetText("Hey ms kek")
+    Console.WriteLine (TextCopy.Clipboard.GetText())
+    0 // return an integer exit code
